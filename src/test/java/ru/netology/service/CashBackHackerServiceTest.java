b5ac071f28f87c4565d572fbd7bd3c65d4f7af03
Andrey Gorkaev <agorkaev99@gmail.com>
2022-09-26 23:47:22 +0300

    Add JUnit and Legacy

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.*;
 
 
 public class CashBackHackerServiceTest {
@@ -17,11 +17,12 @@ public class CashBackHackerServiceTest {
     }
 
     @Test
-    public void amountEqualsBoudary() {
+    public void amountEqualsBoundary() {
         CashBackHackerService service = new CashBackHackerService();
 
         int expected = 0;
         int actual = service.remain(1000);
+
         assertEquals(expected, actual);
     }
 
@@ -32,6 +33,35 @@ public class CashBackHackerServiceTest {
         int expected = 800;
         int actual = service.remain(1200);
 
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountLessThanBoundary2() {
+        CashBackHackerService service = new CashBackHackerService();
+
+        int expected = 200;
+        int actual = service.remain(800);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountEqualsBoundary2() {
+        CashBackHackerService service = new CashBackHackerService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountMoreThanBoundary2() {
+        CashBackHackerService service = new CashBackHackerService();
+
+        int expected = 800;
+        int actual = service.remain(1200);
         assertEquals(expected, actual);
     }
 }
